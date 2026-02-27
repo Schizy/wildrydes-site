41225ab422f44313f664a6ed7f4d0c2a5161bf21
Mechichi Ali <ali.mechichi@gmail.com>
2026-02-27 13:41:20 +0100

    New Cognito App integration (SPA)
    Updated userPoolClientId in the configuration.

--- js/config.js
+++ js/config.js
@@ -1,11 +1,12 @@
 window._config = {
     cognito: {
         userPoolId: 'eu-west-3_8V0jYqhVP',
-        userPoolClientId: '27muqle2249fvpnh0ud93jgo31',
+        userPoolClientId: '4dnnorpeodpb3kr0tpr4qobml6',
         region: 'eu-west-3'
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
     }
 };
+
 
